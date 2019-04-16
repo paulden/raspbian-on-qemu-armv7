3845bcafc051de53c0f8545d0afcac239c4411f1
constant.bridon <cbridon@octo.com>
2019-04-16 12:47:40 +0200

    refacto: ansible playbook for pi setting

---
+++ snippet_integration.py
@@ -0,0 +1,18 @@
+# Take care of paths for execution to work on the pi
+
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+
+model = load_model('/path/to/model_tres_performant.h5')
+img_car = Image.open('/path/to/car.jpg')
+img_car_array = np.array(img_car).reshape((1, 128, 128, 3))
+
+img_not_car = Image.open('/path/to/not_car.jpg')
+img_not_car_array = np.array(img_car).reshape((1, 128, 128, 3))
+
+# Should output 0
+print(model.precit_classes(img_car_array))
+
+# Should output 1
+print(model.precit_classes(img_not_car_array))
